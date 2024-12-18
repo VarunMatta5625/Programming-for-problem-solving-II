7b8af3390e1f42d01ce49413dfad1fbdabd96271
Matta Simhadri Srinivasa Varun <vmatta@student.gitam.edu>
2024-12-18 04:17:42 +0000

    done

---
+++ ArithmeticFuntions.py
@@ -0,0 +1,28 @@
+x = int (input ("Enter value of x\n"))
+y = int (input ("Enter value of y\n"))
+
+
+def add (x,y):
+    return x+y
+
+def diff (x,y):
+    return x-y
+
+def pro (x,y):
+    return x*y
+
+def division (x,y):
+    return x/y
+
+def quotient (x,y):
+    return x//y
+
+def rem (x,y):
+    return x%y
+
+print(f'The sum is {add(x,y)}')
+print(f'The difference is {diff(x,y)}')
+print(f'The product is {pro(x,y)}')
+print(f'The quotient (floating point) is {division(x,y):.2f}')
+print(f'The quotient is {quotient(x,y)}')
+print(f'The remiander when division is performed between {x} and {y} is {rem(x,y)}')
